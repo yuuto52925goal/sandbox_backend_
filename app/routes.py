a374c399067418c448e2ed6e4cf9809749f09782
yuuto52925goal <yuuto52925goal@gmail.com>
2025-03-13 04:48:03 -0600

    Process db

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,14 @@
-from app import app
+from flask import Blueprint, jsonify
+from flask import Blueprint, current_app, jsonify
 
-@app.route('/')
+main = Blueprint('main', __name__)
+
+@main.route('/users', methods=['GET'])
 def home():
-    return "Hello, World!"
+    try:
+        supabase = current_app.config.get('SUPABASE')
+
+        response = supabase.table('company').select('*').execute()
+        return jsonify(response.data)
+    except Exception as e:
+        return jsonify({"error": str(e)})
